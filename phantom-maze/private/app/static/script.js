6c2c059b40de5680407b147450716c52f84badb9
Nikita Sychev <root@nsychev.ru>
2018-07-28 18:02:34 +0000

    fix minor issues

--- phantom-maze/private/app/static/script.js
+++ phantom-maze/private/app/static/script.js
@@ -7,7 +7,7 @@ addEventListener('DOMContentLoaded', () => {
         let data = new FormData();
         data.append('id', getCurrentLocation());
         const pars = { method: 'POST', body: data };
-        fetch('http://127.0.0.1:5000/next', pars).then((r) => {
+        fetch('/next', pars).then((r) => {
             return r.text();
         }).then((r) => {
             document.getElementsByTagName('a')[0].href = r;
