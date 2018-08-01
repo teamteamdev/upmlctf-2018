9249b9a6bf6960387e74af40ba9a1e4e934e7209
Nikita Sychev <root@nsychev.ru>
2018-08-01 13:10:25 +0500

    add dependency to secrets task

--- secrets/public/bot.py
+++ secrets/public/bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# requires python-telegram-bot package from pip
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
